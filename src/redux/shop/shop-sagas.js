2f790b6f784a4b1fcc54d3b13bf2549595d29265
Marko <msamoilovic@gmail.com>
2020-05-02 15:03:50 +0200

    Dodata Redux saga za hvatanje kategorija

--- src/redux/shop/shop-sagas.js
+++ src/redux/shop/shop-sagas.js
@@ -1,11 +1,22 @@
-import {takeEvery} from 'redux-saga/effects'
+import { takeEvery, call, put } from "redux-saga/effects";
+import {
+  firestore,
+  convertSnapshotToObject,
+} from "../../firebase/firebase-utils";
 
-
+import { fetchCollectionSuccess, fetchCollectionFailure } from "./shop-actions";
 
 export function* fetchCollectionAsync() {
-    yield console.log('I am fired')
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(convertSnapshotToObject, snapshot);
+    yield put(fetchCollectionSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionFailure(error.message));
+  }
 }
 
 export function* fetchCollectionStart() {
-    yield takeEvery('FETCH_COLLECTION_START', fetchCollectionAsync)
+  yield takeEvery("FETCH_COLLECTION_START", fetchCollectionAsync);
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,24 +2,27 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 //import thunk from 'redux-thunk'
 import { persistStore } from "redux-persist";
-import createSagaMiddleware from 'redux-saga'
+import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./main-reducer";
 
-import {fetchCollectionStart} from '../redux/shop/shop-sagas'
+import { fetchCollectionStart } from "../redux/shop/shop-sagas";
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 
 const middleWare = [sagaMiddleware];
 
-if(process.env.NODE_ENV === 'development') {
-    middleWare.push(logger)
+if (process.env.NODE_ENV === "development") {
+  middleWare.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middleWare));
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleWare))
+);
 
-sagaMiddleware.run(fetchCollectionStart) 
+sagaMiddleware.run(fetchCollectionStart);
 
 export const persistor = persistStore(store);
 
-/* export default { store, persistor } */;
-  
+/* export default { store, persistor } */
