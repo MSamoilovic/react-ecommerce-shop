d2c8adc25be6a33a1f9499fba5510031bb9a2509
Marko <msamoilovic@gmail.com>
2020-05-27 17:14:19 +0200

    removed typpo on the CartIcon component

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -12,7 +12,7 @@ const mapStateToProps = (state) => ({
 
 export const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
-    <div className="cart-items">u
+    <div className="cart-items">
       {cartItems.length ? (
         cartItems.map((cartItem) => (
           <CartItem key={cartItem.id} item={cartItem} />
